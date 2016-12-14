94ac721252c5a55cdd218bdafb9c68738d62ca28
Julien Esposito <julien.esposito@gmail.com>
2016-12-14 08:23:24 -0800

    JS Code
    JS Code

---
+++ week01/public_html/week3.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function week3()
+{
+    //window.alert( " Hey ");
+    var someArray = new Array();
+    
+    for( var i = 0; i < 10; i++ )
+    {
+        someArray[ i ] = i;
+        document.writeln( someArray[ i ] + "A</br>" );
+
+    }
+}
